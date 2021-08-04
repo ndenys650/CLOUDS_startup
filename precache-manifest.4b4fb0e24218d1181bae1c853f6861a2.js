45e1ce45c7f0b64324c3c244cc6983eb83acb3c1
Nate Denys <nate_denys@yahoo.com>
2021-08-04 11:06:35 -0700

    initial upload with foundations complete/PWA implement

---
+++ precache-manifest.4b4fb0e24218d1181bae1c853f6861a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a74b00d4125df9925d7",
+    "url": "/CLOUDS_startup/css/about.42396e64.css"
+  },
+  {
+    "revision": "9f310888f186fa998a05",
+    "url": "/CLOUDS_startup/css/app.1ad71986.css"
+  },
+  {
+    "revision": "ff0be39d5b24a9ebaf9229434d4f70eb",
+    "url": "/CLOUDS_startup/index.html"
+  },
+  {
+    "revision": "9a74b00d4125df9925d7",
+    "url": "/CLOUDS_startup/js/about.313dbeae.js"
+  },
+  {
+    "revision": "9f310888f186fa998a05",
+    "url": "/CLOUDS_startup/js/app.14c6aac5.js"
+  },
+  {
+    "revision": "93daf63dc4dda1395fe9",
+    "url": "/CLOUDS_startup/js/chunk-vendors.aef2ca12.js"
+  },
+  {
+    "revision": "9b8507a679a6c9b0aa923435906f00de",
+    "url": "/CLOUDS_startup/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CLOUDS_startup/robots.txt"
+  }
+]);
